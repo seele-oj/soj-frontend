a2912ffefe07f71900bb35dabb7e2739563691db
Yeri Kim <laplacianmatrix@gmail.com>
2025-02-09 14:19:29 +0900

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	let lib: typeof import('soj-wasm');
+
+	onMount(async () => {
+		lib = await import('soj-wasm');
+		await lib.default();
+		lib.greet();
+	});
+</script>
+
+<button on:click={() => lib.greet()}>Greet</button>
